9371e2826aa3b188a3e9274fbf7851da78940c29
travensolli <gabriel_t_silva@hotmail.com>
2025-05-04 19:26:37 -0300

    Deletando arquivo config

--- src/config.py
+++
@@ -1,9 +0,0 @@
-# Adicionei o carregamento das variáveis de ambiente do arquivo .env
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-# Substitua as variáveis sensíveis por chamadas ao os.getenv
-API_KEY = os.getenv('API_KEY')
-DATABASE_URL = os.getenv('DATABASE_URL')
